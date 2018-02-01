38aa0e6c73a823fda7f694c2364395429c314ce5
Ben Willis <benwillis0612@gmail.com>
2018-02-01 21:04:51 +0000

    Updated menu formating and images

--- routes/info.js
+++ routes/info.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET the menus page */
 router.get('/menus', function (req, res, next){
-	start = new Date(2017, 1-1, 15);
+	start = new Date(2018, 1-1, 15);
 	now = new Date();
 	week = 7*24*60*60*1000;
 	currWeek = (req.query.week) ? parseInt(req.query.week) : Math.floor((now-start)/week) + 1;
