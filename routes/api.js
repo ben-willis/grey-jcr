eb69d49fecebd713df20d2cf78412878595737f0
Ben Willis <benwillis0612@gmail.com>
2018-03-04 12:06:58 +0000

    Remove treeize requirements

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var prettydate = require('pretty-date');
-var treeize   = require('treeize');
 var httpError = require('http-errors');
 
 var User = require('../models/user');
