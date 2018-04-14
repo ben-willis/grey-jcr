08fc14c3da1b7047f773ac3a4f6161a5842b9807
Ben Willis <benwillis0612@gmail.com>
2018-04-14 14:43:37 +0100

    Initiate sequelize

---
+++ src/helpers/sequelize.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  operatorsAliases: false,
+
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  define: {
+    timestamps: false,
+    underscored: true
+  }
+});
+
+module.exports = sequelize;
