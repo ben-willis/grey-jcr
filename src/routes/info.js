8f2603c3dab9d35fc54f9ae6fdda5553c8e99729
Daniel Nagy <danielnagy69@gmail.com>
2018-10-21 16:28:30 +0100

    Add new dinner menus
    Add new menu images and set term start date.

--- src/routes/info.js
+++ src/routes/info.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET the menus page */
 router.get('/menus', function (req, res, next){
-	start = new Date(2018, 3, 23);
+	start = new Date(2018, 9, 1);
 	now = new Date();
 	week = 7*24*60*60*1000;
 	currWeek = (req.query.week) ? parseInt(req.query.week) : Math.floor((now-start)/week) + 1;
