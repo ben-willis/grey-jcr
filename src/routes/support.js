f17185cfa436be11fc3cb5a4352e054816b13ab0
Daniel Nagy <danielnagy69@gmail.com>
2018-12-21 17:36:44 +0000

    Handle errors related to fetching roles for role cards on Support page.

--- src/routes/support.js
+++ src/routes/support.js
@@ -7,28 +7,41 @@ var Role = require('../models/role');
 router.get('/', function (req, res, next) {
 		Promise.all([
 			Role.getByType("rep").then(function(roles) {
-				return Promise.all(
-					roles.map(function(role) {
-						return role.getUsers().then(function(users) {
-							role.users = users;
-							return role;
-						});
-					})
-				);
+				if (typeof roles !== 'undefined') {
+					return Promise.all(
+						roles.map(function(role) {
+							return role.getUsers().then(function(users) {
+								role.users = users;
+								return role;
+							});
+						})
+					);
+				}
+				return [];
 			}),
 			Role.getByType("exec").then(function(roles) {
-				const reps_officer = roles.find(role => role.title == "Representatives Officer");
-				return reps_officer.getUsers().then(function(users) {
-					reps_officer.users = users;
-					return reps_officer;
-				});
+				if (typeof roles !== 'undefined') {
+					const reps_officer = roles.find(role => role.title == "Representatives Officer");
+					if (typeof reps_officer !== 'undefined') {
+						return reps_officer.getUsers().then(function(users) {
+							reps_officer.users = users;
+							return reps_officer;
+						});
+					}
+				}
+				return null;
 			}),
 			Role.getByType("officer").then(function(roles) {
-				const sf_rep = roles.find(role => role.title == "Senior Freshers' Representative");
-				return sf_rep.getUsers().then(function(users) {
-					sf_rep.users = users;
-					return sf_rep;
-				});
+				if (typeof roles !== 'undefined') {
+					const sf_rep = roles.find(role => role.title == "Senior Freshers' Representative");
+					if (typeof sf_rep !== 'undefined') {
+						return sf_rep.getUsers().then(function(users) {
+							sf_rep.users = users;
+							return sf_rep;
+						});
+					}
+				}
+				return null;
 			})
 		]).then(function (roles) {
 			res.render('support/index', {reps: roles[0], reps_officer: roles[1], sf_rep: roles[2]});
