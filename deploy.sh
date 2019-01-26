d852ee87613d77cea3e7661941a0402a1139bea6
Ben Willis <benwillis0612@gmail.com>
2019-01-26 14:07:07 +0000

    Update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-cd grey-jcr
-docker load -i ./greyjcr_app.tar
-docker-compose up -d
-docker-compose run app npm run migrate-postgres
+cd grey-jcr-test
+
+rm -rf package
+tar -xf grey-jcr.tgz
+cp .env package/
+cd package
+nvm use
+npm install
+npm run migrate-postgres
+
+# Restart the new app
+pm2 delete grey-jcr
+pm2 start ./bin/www -n grey-jcr
