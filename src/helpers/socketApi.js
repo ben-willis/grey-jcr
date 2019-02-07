edff7544a4b31fe48450512a60fb259a34559317
Ben Willis <benwillis0612@gmail.com>
2019-02-07 20:51:58 +0000

    Set up socket io redis adapter (#86)

--- src/helpers/socketApi.js
+++ src/helpers/socketApi.js
@@ -1,5 +1,13 @@
 var socket_io = require('socket.io');
 var io = socket_io();
+
+const redisAdapter = require("socket.io-redis")({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
+
+io.adapter(redisAdapter);
+
 var socketApi = {};
 
 socketApi.io = io;
