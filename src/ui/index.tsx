d776528951020a12a211dbe103df10da520fcd72
Ben Willis <benwillis0612@gmail.com>
2019-02-08 23:21:59 +0000

    Add news feed to home page

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom";
 
 import EventsCalendar from "./events/EventsCalendar";
 
+import NewsFeed from "./news/NewsFeed";
+
 const now = new Date();
-ReactDOM.render(
-    <EventsCalendar initialYear={now.getFullYear()} initialMonth={now.getMonth() + 1}/>,
-    document.getElementById("events-calendar"),
-);
+if (document.getElementById("events-calendar")) {
+    ReactDOM.render(
+        <EventsCalendar initialYear={now.getFullYear()} initialMonth={now.getMonth() + 1}/>,
+        document.getElementById("events-calendar"),
+    );
+}
+
+if (document.getElementById("news-feed")) {
+    ReactDOM.render(
+        <NewsFeed/>,
+        document.getElementById("news-feed"),
+    );
+}
