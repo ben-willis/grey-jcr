fee3b4dc5aa022a97f0562fb7be4236496e68f51
Ben Willis <benwillis0612@gmail.com>
2019-02-20 13:05:09 +0000

    Add file service interface

---
+++ src/files/FileService.ts
@@ -0,0 +1,10 @@
+import Folder from "./entities/Folder";
+import File from "./entities/File";
+
+export default interface FileService { 
+    getFile(fileId: number): Promise<File>;
+    getFolder(folderId: number): Promise<Folder>;
+    createFolder(name: string, parentId?: number, owner?: number): Promise<Folder>;
+    uploadFile(name: string, tmpPath: string, folderId: number, description?: string): Promise<File>;
+    deleteFileOrFolder(fileOrFolder: "FOLDER"|"FILE", id: number): Promise<void>;
+}
