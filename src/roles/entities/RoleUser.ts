360d6d0d616a5f6c7eca8a6d195aa939fee7d494
Ben Willis <benwillis0612@gmail.com>
2019-02-21 21:28:46 +0000

    Add role models

---
+++ src/roles/entities/RoleUser.ts
@@ -0,0 +1,17 @@
+import {Entity, JoinColumn, ManyToOne, PrimaryColumn} from "typeorm";
+import Role from "./Role";
+
+@Entity({
+    name: "user_roles",
+})
+export default class RoleUser {
+    @PrimaryColumn({type: "varchar", length: 6})
+    public username: string;
+
+    @PrimaryColumn({type: "integer", name: "role_id"})
+    public roleId: number;
+
+    @ManyToOne((type) => Role, (role) => role.roleUsers, {onDelete: "CASCADE"})
+    @JoinColumn({name: "role_id"})
+    public role?: Role;
+}
