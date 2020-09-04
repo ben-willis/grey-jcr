bf1ecde7d9848b8e0d58e54e36673fc8ce394cf2
Amy Hudspith <amyhudspith999@gmail.com>
2020-09-04 16:28:53 +0100

    Make handbook public

--- src/routes/handbook.js
+++ src/routes/handbook.js
@@ -2,15 +2,7 @@ var express = require('express');
 var router = express.Router();
 var httpError = require('http-errors');
 
-router.use(function (req, res, next) {
-	//Check User Authenticated
-	if (!req.isAuthenticated()) {
-		req.session.redirect_to = req.originalUrl;
-		return res.redirect(401, '/login?unauthorised');
-	} else {
-		return next();
-	}
-});
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
